07a3b597652130348ae968fce446c7bab38bf7e8
musseGkel <musseg437@gmail.com>
2026-02-16 14:15:28 +0100

    Added InterviewDomain enum to define various interview topics

---
+++ backend/interview/src/main/java/com/smartmock/interview/domain/InterviewDomain.java
@@ -0,0 +1,16 @@
+package com.smartmock.interview.domain;
+
+public enum InterviewDomain {
+    JAVA,
+    PYTHON,
+    SYSTEM_DESIGN,
+    DATABASE,
+    ALGORITHM,
+    DATA_STRUCTURE,
+    OPERATING_SYSTEM,
+    NETWORK,
+    COMPILER_CONSTRUCTION,
+    ARTIFICIAL_INTELLIGENCE,
+    MACHINE_LEARNING,
+    DEEP_LEARNING,
+}
