eb648276a7021d2824d28ac70468d24e933dd5d0
musseGkel <musseg437@gmail.com>
2026-03-16 15:45:11 +0100

    Add ConfigurationPropertiesScan annotation to InterviewApplication for property scanning

--- backend/interview/src/main/java/com/smartmock/interview/InterviewApplication.java
+++ backend/interview/src/main/java/com/smartmock/interview/InterviewApplication.java
@@ -2,8 +2,10 @@ package com.smartmock.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class InterviewApplication {
 
 	public static void main(String[] args) {
